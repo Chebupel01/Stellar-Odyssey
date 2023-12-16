02d0b36550916248e339091d2f01a14092c28dea
Kirill <f1l1ppovc@yandex.ru>
2023-12-16 19:58:29 +0300

    создал окно

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+size = width, height = 500, 300
+screen = pygame.display.set_mode(size)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
