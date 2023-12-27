010aca642c7d766567d7a396a2c6caae3415d321
Chebupel01 <148780196+Chebupel01@users.noreply.github.com>
2023-12-27 22:13:29 +0300

    Update main.py
    Основа программы

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pygame
+import sys
+from PIL import Image
+
+SCREEN_WIDTH = 1800
+SCREEN_HEIGHT = 893
+WHITE = (255, 255, 255)
 
 
-size = width, height = 500, 300
-screen = pygame.display.set_mode(size)
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Stellar Odyssey")
+pygame.init()
+background_image = pygame.image.load("data/BG/BG.png").convert()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    screen.fill(WHITE)
+    screen.blit(background_image, (0, 0))
+    pygame.display.update()
     pygame.display.flip()
+pygame.quit()
+sys.exit()
