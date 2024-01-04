77ab2e70e97dbb3ac763028a2cd563917f3ef7c6
Kirill <f1l1ppovc@yandex.ru>
2024-01-04 11:41:54 +0300

    добавил функцию для атаки снеговика

--- main.py
+++ main.py
@@ -1,49 +1,81 @@
 import pygame
-import sys
-from PIL import Image
 
 SCREEN_WIDTH = 1800
 SCREEN_HEIGHT = 893
 WHITE = (255, 255, 255)
 clock = pygame.time.Clock()
-r_image = pygame.image.load('data/snowman/снеговик.png')
-l_image = pygame.image.load('data/snowman/снеговик2.png')
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
 
-class enemy(object):
+class Enemy(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.speed = 8
         self.start = x
         self.end = SCREEN_WIDTH - 200
+        self.last_shoot_time = pygame.time.get_ticks()
+        self.left_image_index = 0
+        self.right_image_index = 0
+        self.left_attack = [pygame.image.load('data/snowman/атака1.png'),
+                            pygame.image.load('data/snowman/атака2.png'),
+                            pygame.image.load('data/snowman/атака3.png'),
+                            pygame.image.load('data/snowman/атака4.png'),
+                            pygame.image.load('data/snowman/атака5.png'),
+                            pygame.image.load('data/snowman/атака6.png'),
+                            pygame.image.load('data/snowman/атака7.png'),
+                            pygame.image.load('data/snowman/атака8.png'),
+                            pygame.image.load('data/snowman/атака9.png'),
+                            pygame.image.load('data/snowman/атака10.png')]
+        self.right_attack = [pygame.image.load('data/snowman/атакаслева1.png'),
+                             pygame.image.load('data/snowman/атакасл2.png'),
+                             pygame.image.load('data/snowman/атакасл3.png'),
+                             pygame.image.load('data/snowman/атакасл4.png'),
+                             pygame.image.load('data/snowman/атакасл5.png'),
+                             pygame.image.load('data/snowman/атакасл6.png'),
+                             pygame.image.load('data/snowman/атакасл7.png'),
+                             pygame.image.load('data/snowman/атакасл8.png'),
+                             pygame.image.load('data/snowman/атакасл9.png'),
+                             pygame.image.load('data/snowman/атакасл10.png')]
+        self.enemy_images = self.left_attack
 
     def move(self):
+        current_time = pygame.time.get_ticks()
+        time_since_last_shoot = current_time - self.last_shoot_time
+        if time_since_last_shoot >= 5000:
+            self.shoot()
+            self.last_shoot_time = current_time
         if self.speed < 0:
             if self.x > self.start - self.speed:
                 self.x += self.speed
             else:
                 self.speed *= -1
                 self.x += self.speed
-            self.enemy = pygame.transform.scale(l_image, (200, 200))
+            self.enemy_images = self.left_attack
         else:
             if self.x < self.end + self.speed:
                 self.x += self.speed
             else:
                 self.speed *= -1
                 self.x += self.speed
-            self.enemy = pygame.transform.scale(r_image, (200, 200))
+            self.enemy_images = self.right_attack
+
+    def shoot(self):
+        if self.speed < 0:
+            self.left_image_index = (self.left_image_index + 1) % len(self.left_attack)
+            self.enemy_images = self.left_attack
+        else:
+            self.right_image_index = (self.right_image_index + 1) % len(self.right_attack)
+            self.enemy_images = self.right_attack
+
+    def get_current_image(self):
+        return pygame.transform.scale(
+            self.enemy_images[self.left_image_index if self.speed < 0 else self.right_image_index], (200, 200))
 
 
-
-
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Stellar Odyssey")
-pygame.init()
-mucus = enemy(0, 500)
+mucus = Enemy(0, 500)
 background_image = pygame.image.load("data/BG/BG.png").convert()
 running = True
 while running:
@@ -53,9 +85,7 @@ while running:
     screen.fill(WHITE)
     screen.blit(background_image, (0, 0))
     mucus.move()
-    screen.blit(mucus.enemy, (mucus.x, mucus.y))
+    screen.blit(mucus.get_current_image(), (mucus.x, mucus.y))
     pygame.display.update()
     clock.tick(30)
-    pygame.display.flip()
 pygame.quit()
-sys.exit()
